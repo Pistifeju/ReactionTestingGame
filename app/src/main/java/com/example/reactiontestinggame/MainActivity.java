5a7fd4bbbb40d4ad48fa1268e52ee13ad17ba0c7
Pistifeju <juhasz.pisti7253@gmail.com>
2023-04-21 23:19:14 +0200

    Creating NavigationBar

--- app/src/main/java/com/example/reactiontestinggame/MainActivity.java
+++ app/src/main/java/com/example/reactiontestinggame/MainActivity.java
@@ -1,31 +1,79 @@
 package com.example.reactiontestinggame;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
-    private FirebaseUser user;
-    private FirebaseAuth auth;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        auth = FirebaseAuth.getInstance();
-        user = auth.getCurrentUser();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        toolbar.showOverflowMenu();
 
-        if (user == null) {
-            startActivity(new Intent(MainActivity.this, LoginActivity.class));
-            finish();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = auth.getCurrentUser();
+
+        if (firebaseUser == null) {
+            logout();
         } else {
-            auth.signOut();
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            String userId = firebaseUser.getUid();
+            DocumentReference userRef = db.collection("users").document(userId);
+
+            userRef.get().addOnSuccessListener(documentSnapshot -> {
+                if (documentSnapshot.exists()) {
+                    user = documentSnapshot.toObject(User.class);
+                    assert user != null;
+                    toolbar.setTitle(user.getUsername());
+                } else {
+                    Toast.makeText(MainActivity.this, "Error happened while fetching your account.", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(e -> {
+                Toast.makeText(MainActivity.this, "Error happened while fetching your account.", Toast.LENGTH_SHORT).show();
+            });
+
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_leaderboard) {
+            // Handle leaderboard action
+            return true;
+        } else if (id == R.id.action_logout) {
+            FirebaseAuth.getInstance().signOut();
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        finish();
     }
 }
